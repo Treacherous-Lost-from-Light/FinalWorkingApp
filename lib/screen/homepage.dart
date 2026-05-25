52e5dad2fbbcf4ea59a12f4770ea15db373d03b7
zeino <bryansesegt@gmail.com>
2026-05-26 06:40:59 +0800

    Polished homepage's UI

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -16,54 +16,62 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final mood = ModalRoute.of(context)!.settings.arguments as String;
+
     return Scaffold(
+    backgroundColor: Color(0xFF060F1A),
+    
      body: Padding(
-      padding: const EdgeInsets.all(40),
+      padding: const EdgeInsets.all(20),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          
+         Container(
+           alignment: Alignment.topLeft,
+           child: Text("Today's mood: $mood",
+                   style: TextStyle(
+            fontSize: 15,
+            color: Color.fromARGB(255, 129, 152, 175)
+                   ),
+                   ),
+         ),
 
-         Text("Today's mood: $mood",
-        style: TextStyle(
-          fontSize: 15,
-        ),
-        ),
-
-        SizedBox(height: 30,),
+        SizedBox(height: 100,),
         
         if(mood == "Happy") ...[
           _moodRecommendationCard(
             context, 
-            "Enjoying life are we?", 
-            "Maintain you mood", 
+            "You're happy today, I'm proud of you", 
+            "Writing helps you to maintain mood.", 
             "Begin writing", 
             '/Breathing')
         ]
         else if(mood == "Normal") ...[
           _moodRecommendationCard(
             context, 
-            "Enjoying life are we?", 
-            "Maintain you mood", 
+            "So a normal day is it?", 
+            "Sometimes we forget to notice the small things that truly matters. Writing down a moment or feeling that lingered for a bit than it should have helps you notice those small things.", 
             "Begin writing", 
             '/Breathing')
         ]
         else if(mood == "Unhappy") ...[
           _moodRecommendationCard(
             context, 
-            "Enjoying life are we?", 
-            "Maintain you mood", 
-            "Begin writing", 
-            '/Breathing')
+            "You seem sad today", 
+            "Writing down what bothers you helps lighten your mood. Talking about it also helps.", 
+            "Talk with Lucas", 
+            '/Chatbot')
         ]
         else if(mood == "Stressed") ...[
           _moodRecommendationCard(
             context, 
-            "Enjoying life are we?", 
-            "Maintain you mood", 
+            "You seemed overwhelmed today.?", 
+            "Take a short pause and breathe. Small moment of calm can help reset you mind.", 
             "Begin writing", 
             '/Breathing')
         ],
 
+        SizedBox(height: 20),
 
         
         GestureDetector(
@@ -77,18 +85,17 @@ class _HomePage extends State<HomePage> {
           },
           child: Text("Daily health tips",
           style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+            fontSize: 15,
+            color: Colors.white,
           ),
           ),
         ),
 
-         const SizedBox(height: 30),
-
          Text(
           healthtips,
           style: TextStyle(
-            fontSize: 13,
+             fontSize: 10,
+             color: Colors.white,
           ),
          )
 
@@ -102,23 +109,31 @@ class _HomePage extends State<HomePage> {
 
   Widget _moodRecommendationCard(BuildContext context, String feeling, String message, String action, String routename) {
     return Container(
-              alignment: Alignment.topLeft,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(11),
-                color: Colors.amber,
-              ),
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+        color: Color(0xFF103A5C), 
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: Color(0xFF103A5C)),
+      ),
         
               //TEXT
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
+
+
                   Text(feeling,
                   style: TextStyle(
-                    color: Colors.black,
+                    color: Colors.white,
                    ),
                   ),
+                  
+                  SizedBox(height: 15),
+
                   Text(message,
                   style: TextStyle(
-                    color: Colors.black,
+                    color: Colors.white,
                    ),
                  ),
 
